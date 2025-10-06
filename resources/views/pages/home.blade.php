b813f778b239bfb2c5d77100986939b91a1d4ddd
chidokato <tuan.pn92@gmail.com>
2025-10-06 08:49:22 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -67,7 +67,7 @@
 
         <div class="load-more text-center mt-4 pt-2">
             <div class="cta-btn ir">
-                <a class="" target="_blank" href="du-an">
+                <a class="" target="_blank" href="projects">
                     <span class="cta-text font-weight-semibold">查看全部</span>
                     <span class="cta-ico"><i class="icon-next"></i></span>
                 </a>
@@ -171,7 +171,7 @@
         </div>
         <div class="load-more text-center mt-4 pt-2">
             <div class="cta-btn ir">
-                <a class="" href="tin-tuc">
+                <a class="" href="news">
                     <span class="cta-text font-weight-semibold">查看全部</span>
                     <span class="cta-ico"><i class="icon-next"></i></span>
                 </a>
