afb7c45555791cff13b7416f40ff2229477e281c
chidokato <tuan.pn92@gmail.com>
2025-10-20 16:55:02 +0700

    update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -11,13 +11,13 @@
                         </div>
                         <span class="ft-contact-item-ico"><i class="icon-phone-filled"></i></span>
                     </div>
-                    <div class="ft-contact-item">
+                    <!-- <div class="ft-contact-item">
                         <div class="ft-contact-item-text">
                             <h6>总部</h6>
                             <span>越南胡志明市Tân Định街Trần Khánh Dư路1号Vạn Gia Phát大厦5楼</span>
                         </div>
                         <span class="ft-contact-item-ico"><i class="icon-building-filled"></i></span>
-                    </div>
+                    </div> -->
                     <div class="ft-contact-item">
                         <div class="ft-contact-item-text">
                             <h6>办公地址</h6>
@@ -42,7 +42,7 @@
                     
                     <div class="col-md-6">
                         <div class="ft-link">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15676.90695339032!2d106.6893047!3d10.7939386!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528cdeb13ffff%3A0x8db7b80bb49f4899!2zQ8O0bmcgVHkgQ-G7lSBQaOG6p24gQuG6pXQgxJDhu5luZyBT4bqjbiBJbmRvY2hpbmU!5e0!3m2!1svi!2s!4v1718358591174!5m2!1svi!2s" style="border:0; width: 100%; height: 300px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            {!! $setting->maps !!}
                         </div>
                     </div>
                 </div>
